28613b9e1084157bd3d960ff730531a941dedc98
Andreas Stahlhofen <astahlhofen@uni-koblenz.de>
2019-12-04 22:42:00 +0100

    Add output_directory option to scantailor-cli including test if the specified directory is valid. If the directory not exists it will be created on the fly.

--- src/cli/CmdParser.h
+++ src/cli/CmdParser.h
@@ -29,25 +29,24 @@
 
 struct CmdOptions {
   std::vector<QFileInfo> inputFiles;
+  QDir outputDirectory;
 };
 
 enum ParserResult { OPTIONS_OK, OPTIONS_ERROR, OPTIONS_VERSION_REQUESTED, OPTIONS_HELP_REQUESTED };
 
 ParserResult parseCommandLine(QCommandLineParser& _parser, CmdOptions* _options, QString* _errorMessage) {
   /* Add command line options. */
-  QCommandLineOption logLevelInfo{{"v", "verbose"}, "Tell me what you are doing."};
-  QCommandLineOption logLevelVerbose{{"d", "debug"}, "Set the verbosity level to max"};
-  logLevelVerbose.setFlags(logLevelVerbose.flags() | QCommandLineOption::ShortOptionStyle);
+  QCommandLineOption logLevelInfoOpt{{"v", "verbose"}, "Tell me what you are doing."};
+  QCommandLineOption logLevelVerboseOpt{{"d", "debug"}, "Set the verbosity level to max"};
+  logLevelVerboseOpt.setFlags(logLevelVerboseOpt.flags() | QCommandLineOption::ShortOptionStyle);
 
-  _parser.addOptions({
-      // Verbosity
-      logLevelInfo,
-      logLevelVerbose,
-  });
+  QCommandLineOption outputDirOpt{{"o", "output"}, "The output directory.", "output_dir"};
 
-  const QCommandLineOption helpOption = _parser.addHelpOption();
-  const QCommandLineOption versionOption{"version", "The version."};
-  _parser.addOption(versionOption);
+  _parser.addOptions({logLevelInfoOpt, logLevelVerboseOpt, outputDirOpt});
+
+  const QCommandLineOption helpOpt = _parser.addHelpOption();
+  const QCommandLineOption versionOpt{"version", "The version."};
+  _parser.addOption(versionOpt);
 
   _parser.addPositionalArgument("input_images", "The images to modify using scan tailor", "[files...]");
 
@@ -59,20 +58,48 @@ ParserResult parseCommandLine(QCommandLineParser& _parser, CmdOptions* _options,
 
   // Parse the log level at the beginning so that we can use it later in parseCommandLine().
   Logger::instance()->setLogLevel(LogLevel::WARNING);
-  if (_parser.isSet(logLevelInfo)) {
+  if (_parser.isSet(logLevelInfoOpt)) {
     Logger::instance()->setLogLevel(LogLevel::INFO);
-  } else if (_parser.isSet(logLevelVerbose)) {
+  } else if (_parser.isSet(logLevelVerboseOpt)) {
     Logger::instance()->setLogLevel(LogLevel::DEBUG);
   }
 
-  if (_parser.isSet(helpOption)) {
+  // Check if help or version is specified.
+  if (_parser.isSet(helpOpt)) {
     return OPTIONS_HELP_REQUESTED;
   }
-
-  if (_parser.isSet(versionOption)) {
+  if (_parser.isSet(versionOpt)) {
     return OPTIONS_VERSION_REQUESTED;
   }
 
+  // Read and check the output directory.
+  if (!_parser.isSet(outputDirOpt)) {
+    *_errorMessage = "You must specify an output directory using -o <output_dir> or --output <output_dir>";
+    return OPTIONS_ERROR;
+  }
+  const QString outputDirStr = _parser.value(outputDirOpt);
+  if (outputDirStr.isEmpty()) {
+    *_errorMessage = "The output argument is empty";
+    return OPTIONS_ERROR;
+  }
+  const QFileInfo outputDirInfo{outputDirStr};
+  if (outputDirInfo.isFile()) {
+    *_errorMessage = "The specified output directory '" + outputDirInfo.absoluteFilePath() + "' is a filed";
+    return OPTIONS_ERROR;
+  }
+  const QDir outputDir{outputDirStr};
+  if (!outputDir.exists()) {
+    if (!QDir{}.mkpath(outputDir.absolutePath())) {
+      *_errorMessage = "Failed to create the output directory '" + outputDir.absolutePath() + "'";
+      return OPTIONS_ERROR;
+    }
+    Logger::debug() << "parseCommandLine(): Create the output directory '" << outputDir.absolutePath().toStdString()
+                    << "'" << Logger::eol;
+  }
+  _options->outputDirectory = outputDir;
+
+
+  // Parse input files as positional arguments.
   QStringList files = _parser.positionalArguments();
   if (files.isEmpty()) {
     *_errorMessage = "No input files specified";
