8429d64e158d6c450788a0e1926fca51f1f34dfc
Andreas Stahlhofen <astahlhofen@uni-koblenz.de>
2019-12-04 12:43:34 +0100

    Add skeleton for the advanced scantailor-cli and integrate it into the build process.

---
+++ src/cli/main.cpp
@@ -0,0 +1,9 @@
+// Copyright (C) 2019  Andreas Stahlhofen <andreas.stahlhofen@gmail.com>
+// Use of this source code is governed by the GNU GPLv3 license that can be found in the LICENSE file.
+
+#include <iostream>
+
+int main(int _argc, char** _argv) {
+  std::cout << "Hello World!" << std::endl;
+  return 0;
+}
